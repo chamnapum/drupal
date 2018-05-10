d291f54263890359d0f7b2decb17485795dfe15d
chamnap um <chamnap.um@gmail.com>
2018-05-10 13:42:53 +0700

    upload files

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload')
+var uglify = require('gulp-uglifyjs');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+
+
+
+gulp.task('imagemin', function () {
+    return gulp.src('./themes/custom/searchengine/images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./themes/custom/searchengine/images'));
+});
+
+
+gulp.task('sass', function () {
+  gulp.src('./themes/custom/searchengine/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./themes/custom/searchengine/css'));
+});
+
+
+gulp.task('uglify', function() {
+  gulp.src('./themes/custom/searchengine/lib/*.js')
+    .pipe(sourcemaps.write('./'))
+    .pipe(uglify('main.js'))
+    .pipe(gulp.dest('./themes/custom/searchengine/js'))
+});
+
+gulp.task('watch', function(){
+    livereload.listen();
+
+    gulp.watch('./themes/custom/searchengine/sass/**/*.scss', ['sass']);
+    gulp.watch('./themes/custom/searchengine/lib/*.js', ['uglify']);
+    gulp.watch(['./themes/custom/searchengine/css/style.css', './themes/custom/searchengine/**/*.twig', './themes/custom/searchengine/js/*.js'], function (files){
+        livereload.changed(files)
+    });
+});
---
+++ themes/custom/searchengine/lib/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+    $('#block-endymion-main-menu li').hover(function(){
+        $(this).find('.sub-menu').toggle();
+    });
+
+});
